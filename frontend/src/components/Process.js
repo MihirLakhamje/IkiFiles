dc4e31db8d77be6e424b0cbd93c1e78d15870b38
mihirlakhamje <lakhamjemihir@gmail.com>
2023-02-18 00:25:29 +0530

    installed tailwind

---
+++ frontend/src/components/Process.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Process() {
+    return (
+        <>
+        <div className='flex justify-center items-center h-screen '>
+            <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Website is under development :)</h1>
+        </div>
+        </>
+    )
+}
