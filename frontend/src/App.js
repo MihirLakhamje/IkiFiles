39893e40f9c20846826f557bc1d359fcf1e9df1e
mihirlakhamje <lakhamjemihir@gmail.com>
2023-02-19 13:27:47 +0530

    Created guest component
    19 Feb - created a landing page and modal

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,16 @@
+import { Container } from 'react-bootstrap';
 import './App.css';
+import Guest from './components/Guest';
+import Header from './headerfooter/Header'
+
 
 function App() {
 	return (
 		<>
+		<Container style={{height:"100vh"}}>
+			<Header />
+			<Guest />
+		</Container>
 		</>
 	);
 }
---
+++ frontend/src/headerfooter/Header.js
@@ -0,0 +1,25 @@
+import React, { useState } from 'react'
+import {Navbar, Container, Button} from 'react-bootstrap'
+import AuthModal from '../utility/AuthModal'
+
+
+export default function Header() {
+    const [showModal, setShowModal] = useState(false);
+    return (
+        <>
+            <Navbar>
+                <Container>
+                    <Navbar.Brand href="#home">IkiFiles</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Navbar.Text>
+                            <Button onClick={()=>setShowModal(true)}>Login / Regiter</Button>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+            <AuthModal show={showModal} onHide={()=>setShowModal(false)}/>
+        </>
+    )
+}
